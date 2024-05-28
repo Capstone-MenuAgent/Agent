9b182a174b590c9ea68bc50aa2ae50e854319243
SeokjinDev <tjrwls1108011@gmail.com>
2024-05-29 01:41:13 +0900

    Feat: Prototype
    Prompt Test

---
+++ codespace/simpleMenuBot.py
@@ -0,0 +1,42 @@
+from langchain_openai import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+template = """
+    사용하는 언어는 한국어로 해줘.
+    너는 사용자의 식사 메뉴를 추천해줘야해.
+    답변 시 식사 메뉴 후보는 1개만 정해서 알려줘.
+
+    사용자 나이 : {age},
+    사용자 성별 : {gender},
+    현재 날씨   : {weather}
+
+    답변 양식은 아래와 같아.
+    [AI] : 
+"""
+
+prompt = ChatPromptTemplate.from_messages([
+        ("system", template),
+        ("human", "{question}"),
+    ])
+model = ChatOpenAI(model='gpt-3.5-turbo', temperature=0.1, api_key=OPENAI_API_KEY)
+
+# prototype : check code
+menuBotChain = prompt | model | StrOutputParser()
+
+
+# Test Code
+if __name__ == '__main__':
+    ans = menuBotChain.invoke(
+        {"question": "점심 메뉴 추천해줘",
+        "age": 25,
+        "gender": "남성",
+        "weather": "맑음"
+        })
+    print(ans)
