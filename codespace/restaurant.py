8b05420077a60f31af638728e0ac78cbb115cb7d
SeokjinDev <tjrwls1108011@gmail.com>
2024-06-13 09:45:19 +0900

    Feat: Prototype Restaurant Get Restaurants List

---
+++ codespace/restaurant.py
@@ -0,0 +1,29 @@
+
+def getRestaurant(loc):
+    # 네이버 지도를 활용하여 해당 지역의 음식 크롤링
+
+    restaurantList = [
+        {
+            "name": "권짬뽕",
+            "category": "중식",
+            "menu": [
+                "짜장", "짬뽕"
+            ]
+        },
+        {
+            "name": "두꺼비네",
+            "category": "한식",
+            "menu": [
+                "순대국밥", "수육국밥"
+            ]
+        },
+        {
+            "name": "타베",
+            "category": "일식",
+            "menu": [
+                "등심돈까스", "가츠동"
+            ]
+        }
+    ]
+
+    return restaurantList
