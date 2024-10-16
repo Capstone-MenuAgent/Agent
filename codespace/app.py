18b67baca12c6c40551e672acccbe3b9d85ff350
SeokjinDev <tjrwls1108011@gmail.com>
2024-10-16 19:53:41 +0900

    [FEAT] 스프링과 연동

--- codespace/app.py
+++ codespace/app.py
@@ -1,21 +1,42 @@
-from flask import Flask, request, redirect
+import requests
+from flask import Flask, request, redirect, jsonify
 from flask_cors import CORS
 from menuAgent import menuRecomment
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/question/', methods=['GET'])
+@app.route('/agent/question', methods=['POST'])
 def question():
     paramsDict = request.args.to_dict()
     query = paramsDict['query']
-    age = paramsDict['age']
-    gender = paramsDict['gender']
+    id = paramsDict['id']
     loc = paramsDict['loc']
+    gender = paramsDict['gender']
+    age = paramsDict['age']
+
+    auth = request.headers.get('Authorization')
     
-    res = menuRecomment(query, age, gender, loc)
-    url = f"http://127.0.0.1:8080/agent/answer?ans={res}"
-    return redirect(url)
+
+    ans = menuRecomment(query, loc, gender, age)
+    res = ans.split("\n")
+    data = {
+                "id": id,
+                "answer": res[0],
+                "addr": res[1]
+            }
+    
+    url = "http://127.0.0.1:8080/api/v1/agent/answer"
+    headers = {
+        "Authorization": auth,
+        "Content-Type": "application/json"
+    }
+    response = requests.get(url, json=data, headers=headers)
+    
+    if response.status_code == 200:
+        return jsonify({"status": "success", "response": response.json()})
+    else:
+        return jsonify({"status": "failed", "error": response.status_code}), response.status_code
 
 if __name__ == '__main__':
     app.run()
--- codespace/menuAgent.py
+++ codespace/menuAgent.py
@@ -24,16 +24,14 @@ template = """
     주변 음식점 : {restaurants}
 
     [Response]
-    나이, 성별, 날씨를 고려해서 음식점 이름, 추천 메뉴, 주소를 알려줘.
+    나이, 성별, 하늘 상태, 기온, 별점, 리뷰수, tag를 고려해서 음식점 이름, 추천 메뉴, 주소를 알려줘.
     추천 메뉴는 음식점에서 한 가지만 정해서 알려줘.
     음식점 이름, 추천 메뉴는 " "로 묶어서 알려줘.
-    답변은 세 줄 작성해줘.
-    첫 줄은 하늘 상태와 기온에 대해 작성해줘.
-    두 번째 줄은 "음식점 이름"의 "추천 메뉴"를 작성해줘.
-    세 번째 줄은 주소 데이터만 작성해줘.
+    답변은 두 줄 작성해줘.
+    첫 줄은 하늘 상태, 기온과 "음식점 이름"의 "추천 메뉴"를 작성해줘.
+    두 번째 줄은 "주소"를 알려줘
     아래는 예시야.
-    하늘은 "하늘 상태" 기온은 "온도"입니다.
-    "음식점 이름"의 "추천 메뉴"를 추천해드릴게요.
+    하늘은 "하늘 상태" 기온은 "온도"입니다. "음식점 이름"의 "추천 메뉴"를 추천해드릴게요.
     "주소"
 """
 
@@ -46,7 +44,7 @@ model = ChatOpenAI(model='gpt-3.5-turbo', temperature=0.1, api_key=OPENAI_API_KE
 # prototype : check code
 menuBotChain = prompt | model | StrOutputParser()
 
-def menuRecomment(question, age, gender, loc):
+def menuRecomment(question, loc, gender, age):
     return menuBotChain.invoke(
             {"question": question,
             "age": age,
